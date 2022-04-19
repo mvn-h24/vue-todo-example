fba0237610f6e13491c218448c86023d54dabda9
mvn-h24 <morning.vecher.night@yandex.ru>
2022-04-19 18:57:50 +0300

    init with eslint and prettier

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<style>
+html, body, #app {
+  width: 100%;
+  min-height: 100vh;
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
