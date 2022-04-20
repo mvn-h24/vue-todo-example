0f743c8087d2cfd76f588304cdf4908d411e72a1
mvn-h24 <morning.vecher.night@yandex.ru>
2022-04-20 07:21:32 +0300

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="text-3xl font-bold underline p-4 inline-block rounded-lg border-2 border-amber-500 block">
+    test project
+  </h1>
 </template>
 
 <style>
+/*noinspection ALL*/
 html, body, #app {
   width: 100%;
   min-height: 100vh;
