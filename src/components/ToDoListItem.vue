d5970834a5377932a71feb076c4892422b30930e
mvn-h24 <morning.vecher.night@yandex.ru>
2022-04-20 12:18:28 +0300

    add ToastMessage.vue add edit mode for ToDoListItem.vue

--- src/components/ToDoListItem.vue
+++ src/components/ToDoListItem.vue
@@ -1,17 +1,93 @@
 <template>
-  <div class="todo-card">
-    <span>{{ title }}</span>
+  <div :class="['todo-card', { 'todo-card-editing': editing }]">
+    <div class="w-full relative">
+      <template v-if="errors">
+        <TransitionGroup name="error-mess">
+          <ToastMessage
+            v-for="(errorMessage, index) in errors"
+            class="absolute right-5"
+            :key="index"
+            :message="errorMessage"
+            :type="'error'"
+          />
+        </TransitionGroup>
+      </template>
+      <template v-if="editing">
+        <textarea
+          ref="EditArea"
+          class="w-full resize-none"
+          :value="title"
+        ></textarea>
+        <button class="blue-btn w-auto" @click="handleEdit">Сохранить</button>
+      </template>
+      <template v-else>
+        <span @click="ToggleEdit">{{ title }}</span>
+      </template>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from "vue";
+import { ref, toRefs, watch } from "vue";
+import ToastMessage from "@app/components/ToastMessage.vue";
 
+//props
 interface ToDoListItemProps {
   title: string;
-  description: string;
 }
 const props = defineProps<ToDoListItemProps>();
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { title, description } = toRefs<ToDoListItemProps>(props);
+const { title } = toRefs<ToDoListItemProps>(props);
+
+//flow control
+const editing = ref(false);
+const ToggleEdit = () => {
+  editing.value = !editing.value;
+};
+const errors = ref<Array<string>>([]);
+enum Errors {
+  titleEmpty = "empty title",
+}
+watch(
+  () => errors.value.length,
+  () => {
+    setTimeout(function () {
+      errors.value = [];
+    }, 3000);
+  },
+  { flush: "post" }
+);
+
+//text area
+const EditArea = ref<HTMLElement | null>(null);
+enum EditActions {
+  titleEdited = "title-edited",
+}
+interface EditEmits {
+  (e: EditActions.titleEdited, editedTitle: string): void;
+}
+const emit = defineEmits<EditEmits>();
+function handleEdit() {
+  if (EditArea.value !== null) {
+    const textarea = EditArea.value as HTMLTextAreaElement;
+    if (textarea.value.length) {
+      emit(EditActions.titleEdited, textarea.value);
+      ToggleEdit();
+    } else {
+      errors.value.push(Errors.titleEmpty);
+    }
+  }
+}
 </script>
+
+<!--suppress CssUnusedSymbol -->
+<style scoped>
+.error-mess-enter-active,
+.error-mess-leave-active {
+  transition: all 0.5s ease;
+}
+.error-mess-enter-from,
+.error-mess-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
