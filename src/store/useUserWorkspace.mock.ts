11225a699941c1297cd8ddb459bafcc2ce2063c8
mvn-h24 <morning.vecher.night@yandex.ru>
2022-04-22 13:00:49 +0300

    add list factory

--- src/store/useUserWorkspace.mock.ts
+++ src/store/useUserWorkspace.mock.ts
@@ -2,57 +2,59 @@ import { ITodoListItem } from "@app/types/todo/ITodoListItem";
 import { ITodoList } from "@app/types/todo/ITodoList";
 import { IUserWorkspace } from "@app/types/workspace/IUserWorkspace";
 
-const TodoList1DataMock: Array<ITodoListItem> = [
-  {
-    title: "Test card #1",
-    description: "Test card #1 description",
-  },
-  {
-    title: "Test card #2",
-    description: "Test card #2 description",
-  },
-  {
-    title: "Test card #3",
-    description: "Test card #3 description",
-  },
-  {
-    title: "Test card #4",
-    description: "Test card #4 description",
-  },
-];
-const TodoList2DataMock: Array<ITodoListItem> = [
-  {
-    title: "weonasda;smd;amsd",
-    description: "Test card #1 description",
-  },
-  {
-    title: "",
-    description: "Test card #2 description",
-  },
-  {
-    title: "asdasdas",
-    description: "Test card #3 description",
-  },
-  {
-    title: "*&T@#*$@#&g3t91sss4",
-    description: "Test card #4 description",
-  },
-];
+const TodoListDataMock = (n?: number): Array<ITodoListItem> =>
+  n
+    ? [
+        {
+          title: "weonasda;smd;amsd",
+          description: "Test card #1 description",
+        },
+        {
+          title: "",
+          description: "Test card #2 description",
+        },
+        {
+          title: "asdasdas",
+          description: "Test card #3 description",
+        },
+        {
+          title: "*&T@#*$@#&g3t91sss4",
+          description: "Test card #4 description",
+        },
+      ]
+    : [
+        {
+          title: "Test card #1",
+          description: "Test card #1 description",
+        },
+        {
+          title: "Test card #2",
+          description: "Test card #2 description",
+        },
+        {
+          title: "Test card #3",
+          description: "Test card #3 description",
+        },
+        {
+          title: "Test card #4",
+          description: "Test card #4 description",
+        },
+      ];
 
 const EditableTodoListMock: ITodoList = {
   title: "Test todo list #1",
   editable: true,
-  items: TodoList1DataMock,
+  items: TodoListDataMock(),
 };
 const NotEditableTodoListMock: ITodoList = {
   title: "Test todo list #2",
   editable: false,
-  items: TodoList2DataMock,
+  items: TodoListDataMock(1),
 };
 const TstTodoListMock: ITodoList = {
   title: "Test todo list #3",
   editable: true,
-  items: TodoList2DataMock,
+  items: TodoListDataMock(0),
 };
 
 export const useUserWorkspaceMock: IUserWorkspace = {
